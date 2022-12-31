48b8f8498bc17abc528835d9096c67aed39f9ff4
Mark Veltzer <mark.veltzer@gmail.com>
2022-12-31 19:26:48 +0200

    no commit message given

--- client.sh
+++ client.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -e
+
+# to install the mssql-cli tool
+# pip install mssql-cli --user
+# pip install cli-helpers --upgrade --user
+
+# to do just one command
 # mssql-cli -S localhost -U sa -P P@ssword1234 -Q "SELECT @@VERSION"
+
+# to get an interactive TUI tool
 mssql-cli -S localhost -U sa -P P@ssword1234 
--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-
+# sudo docker pull mcr.microsoft.com/mssql/server:2022-latest
 sudo docker run\
 	-e "ACCEPT_EULA=Y"\
 	-e "MSSQL_SA_PASSWORD=P@ssword1234"\
