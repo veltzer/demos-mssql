cf213c8353364831e2cf8b741a1e35396ba32728
Mark Veltzer <mark.veltzer@gmail.com>
2023-01-03 15:45:10 +0200

    no commit message given

--- client.sh
+++ client.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 # to install the mssql-cli tool
-# pip install mssql-cli --user
-# pip install cli-helpers --upgrade --user
+pip install mssql-cli --user
+pip install cli-helpers --upgrade --user --force
 
 # to do just one command
 # mssql-cli -S localhost -U sa -P P@ssword1234 -Q "SELECT @@VERSION"
